77c70d0bd7f741dbc8aec196002f53429baa8f30
TonyShikhovtsev <ssvsr2@gmail.com>
2023-07-16 20:21:13 +0500

    str_func.py( Функция делает заглавными первые буквы каждого слова в строке)

--- str_func.py
+++ str_func.py
@@ -3,3 +3,9 @@ def uppercase_string(self, input_string):
     Функция ввода строки и возврата заглавными символами
     """
     return input_string.upper()
+
+def capitalize_words(self, input_string):
+    """
+    Функция делает заглавными первые буквы каждого слова в строке
+    """
+    return input_string.title()
